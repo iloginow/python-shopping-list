eea66e86abd78b5444536c99278c8ebab42c9b9b
Ilia Loginov <i.loginow@outlook.com>
2018-01-02 21:06:44 +0300

    Create ToDo comments

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,5 @@
-my_list = []
-
-while 'DONE' not in my_list:
-    my_list.append(input('Add to the list: '))
-
-my_list.remove('DONE')
-
-for thing in my_list:
-    print(thing, sep='\n')
+# make a list to hold onto our our items
+# print out instructions on how to use the app
+# ask for new items
+# add new items to our list
+# be able to quit the app
