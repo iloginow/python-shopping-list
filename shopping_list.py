44553f263b2955643370e9a0a389d1a2c6cc175d
Ilia Loginov <i.loginow@outlook.com>
2018-01-02 21:14:41 +0300

    Print out instructions on how to use the app

--- shopping_list.py
+++ shopping_list.py
@@ -1,7 +1,10 @@
-# make a list to hold onto our our items
+# make a list to hold onto our items
 shopping_list = []
 
 # print out instructions on how to use the app
+print("What should we pick up at the store?")
+print("Enter 'DONE' to stop adding items.")
+
 # ask for new items
 # add new items to our list
 # be able to quit the app
